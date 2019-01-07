49cfa7701c2347bcaacae0f09d2dedd2f83f5673
Thewildcard-pt <31257166+Thewildcard-pt@users.noreply.github.com>
2019-01-07 01:04:00 +0000

    Ecrã Inicial
    really shitty one

---
+++ app/src/main/java/ipca/random/computaomovel/GameActivity.java
@@ -0,0 +1,4 @@
+package ipca.random.computaomovel;
+
+public class GameActivity {
+}
--- app/src/main/java/ipca/random/computaomovel/MainActivity.java
+++ app/src/main/java/ipca/random/computaomovel/MainActivity.java
@@ -1,16 +1,33 @@
 package ipca.random.computaomovel;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    //testing
-    int test = 1;
+
+
+    Button btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+
+        btn_start = findViewById(R.id.btn_start);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
