cbb52f9c99abb2beecd96fae50d376fa49539243
Thewildcard-pt <31257166+Thewildcard-pt@users.noreply.github.com>
2019-01-07 01:13:25 +0000

    test
    i think

--- app/src/main/java/ipca/random/computaomovel/GameActivity.java
+++ app/src/main/java/ipca/random/computaomovel/GameActivity.java
@@ -1,4 +1,28 @@
 package ipca.random.computaomovel;
 
-public class GameActivity {
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+        //shit goes here
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        /*
+        gameView = new GameView(this, size.x, size.y);
+        setContentView(gameView);
+        */
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+
+    }
 }
---
+++ app/src/main/java/ipca/random/computaomovel/GameView.java
@@ -0,0 +1,18 @@
+package ipca.random.computaomovel;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameView extends SurfaceView implements Runnable{
+
+
+}
