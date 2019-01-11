04de661f39f40b0265de1b870a142ef4531ce585
Kikbow Zutachi <mutecanon@hotmail.com>
2019-01-11 23:44:35 +0000

    Added player visuals and a walking animation

--- app/src/main/java/ipca/random/computaomovel/Player.java
+++ app/src/main/java/ipca/random/computaomovel/Player.java
@@ -3,29 +3,41 @@ package ipca.random.computaomovel;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class Player {
-
-    Bitmap bitmap;
+    Bitmap[] spriteList = new Bitmap[5];
+    int currentSprite;
 
     int x;
     int y;
 
-    int currentState;
-    Timer stateLength;
-
     public Player(Context context){
-        currentState = 0;
+        spriteList[0] = BitmapFactory.decodeResource(context.getResources(), R.drawable.player_walk1);
+        spriteList[1] = BitmapFactory.decodeResource(context.getResources(), R.drawable.player_walk2);
+        //spriteList[2] = BitmapFactory.decodeResource(context.getResources(), R.drawable.player_dodgeleft);
+        //spriteList[3] = BitmapFactory.decodeResource(context.getResources(), R.drawable.player_dodgeright);
+        //spriteList[4] = BitmapFactory.decodeResource(context.getResources(), R.drawable.player_jump);
 
-        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.player);
+        // Initialize the walk animator timer
+        Timer updateTimer = new Timer("Walk animation");
+        updateTimer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                if(currentSprite == 0)
+                    currentSprite = 1;
+                else if (currentSprite == 1)
+                    currentSprite = 0;
+            }
+        }, 0, 250);
     }
 
     public void Update(int canvas_width, int canvas_height){
-        x = (canvas_width / 2) - (bitmap.getWidth() / 2);
-        y = canvas_height - bitmap.getHeight();
-    }
 
+
+        x = (canvas_width / 2) - (spriteList[currentSprite].getWidth() / 2);
+        y = canvas_height - spriteList[currentSprite].getHeight() - 50;
+
+    }
 }
