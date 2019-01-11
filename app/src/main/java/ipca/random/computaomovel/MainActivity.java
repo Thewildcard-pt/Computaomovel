d36e27f5d799ea154b9badc6a10303c1e605d643
Thewildcard-pt <31257166+Thewildcard-pt@users.noreply.github.com>
2019-01-11 19:38:29 +0000

    Firebase Conection e envio de variável de teste para establecer se dá para comunicar

--- app/src/main/java/ipca/random/computaomovel/MainActivity.java
+++ app/src/main/java/ipca/random/computaomovel/MainActivity.java
@@ -7,14 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_start;
     Button btn_quit;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Teste");
+        myRef.setValue("Hello, World!");
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
