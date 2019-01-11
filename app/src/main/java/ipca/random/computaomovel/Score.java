d03edd9d04139387f0473cc2df5d5cde365bdafe
Thewildcard-pt <31257166+Thewildcard-pt@users.noreply.github.com>
2019-01-11 23:37:34 +0000

    Criação da classe de pontuação

---
+++ app/src/main/java/ipca/random/computaomovel/Score.java
@@ -0,0 +1,13 @@
+package ipca.random.computaomovel;
+
+public class Score {
+
+    int value;
+    String id;
+
+    public Score(int score, String nome)
+    {
+     value  = score;
+     id = nome;
+    }
+}
