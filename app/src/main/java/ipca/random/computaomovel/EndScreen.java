2a89cc311f246c16f9f5c17b5f982f64670aa8cb
Thewildcard-pt <31257166+Thewildcard-pt@users.noreply.github.com>
2019-01-12 21:33:41 +0000

    Receber os Highscores da firebase

--- app/src/main/java/ipca/random/computaomovel/EndScreen.java
+++ app/src/main/java/ipca/random/computaomovel/EndScreen.java
@@ -3,11 +3,33 @@ package ipca.random.computaomovel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EndScreen extends AppCompatActivity {
+
+    List<Score> Highscores = new ArrayList<Score>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
+
+        //Receber a tabela de Highscores da firebase;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference tabela = database.getReference("Pontuacao");
+        for(int i = 0; i<10;i++){
+            //variaveis usadas Para criar os spots
+            String id;
+            int Score;
+            DatabaseReference x;
+            x = tabela.child(Integer.toString(i));
+            id = x.child("id").toString();
+            Score = Integer.parseInt(x.child("score").toString());
+            Highscores.add(new Score(Score,id));
+        }
     }
 }
